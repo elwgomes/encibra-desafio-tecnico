8f92419f11d7589569f9045dd31feb16061cff0a
Leonardo <elwgomes@icloud.com>
2024-10-16 08:07:19 -0300

    add: packages, config h2, config model mapper

--- src/main/java/br/encibra/desafio/DesafioApplication.java
+++ src/main/java/br/encibra/desafio/DesafioApplication.java
@@ -1,13 +1,17 @@
 package br.encibra.desafio;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class DesafioApplication {
 
 	public static void main(String[] args) {
+		log.info("Starting application");
 		SpringApplication.run(DesafioApplication.class, args);
+		log.info("Application started");
 	}
 
 }
