41cfd76a8a7655c249900663d1453cd468bef52a
Leonardo <elwgomes@icloud.com>
2024-10-16 07:59:33 -0300

    initial commit

---
+++ src/test/java/br/encibra/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.encibra.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
